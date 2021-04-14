42e558b6a9e6dacd8a40be9df4ca83783e8d1b25
Mark Richman <mrkrchm@amazon.com>
2021-04-14 15:33:02 -0400

    Added GraphQL; no auth

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Serverless Notes</h1>
+    <input type="text" v-model="title" placeholder="Note title" />
+    <input type="text" v-model="body" placeholder="Note body" />
+    <button v-on:click="createNote">Create Note</button>
+    <div v-for="item in notes" :key="item.id">
+      <h3>{{ item.title }}</h3>
+      <p>{{ item.body }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from "aws-amplify";
+import { createNote } from "./graphql/mutations";
+import { listNotes } from "./graphql/queries";
+import { onCreateNote } from "./graphql/subscriptions";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  async created() {
+    this.getNotes();
+    this.subscribe();
+  },
+  data() {
+    return {
+      title: "",
+      body: "",
+      notes: [],
+    };
+  },
+  methods: {
+    async createNote() {
+      const { title, body } = this;
+      if (!title || !body) return;
+      const note = { title, body };
+      await API.graphql({
+        query: createNote,
+        variables: { input: note },
+      });
+      this.title = "";
+      this.body = "";
+    },
+    async getNotes() {
+      const notes = await API.graphql({
+        query: listNotes,
+      });
+      this.notes = notes.data.listNotes.items;
+    },
+    subscribe() {
+      API.graphql({ query: onCreateNote }).subscribe({
+        next: (eventData) => {
+          let note = eventData.value.data.onCreateNote;
+          if (this.notes.some((item) => item.title === note.title)) return; // remove duplications
+          this.notes = [...this.notes, note];
+        },
+      });
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNote = /* GraphQL */ `
+  subscription OnCreateNote {
+    onCreateNote {
+      id
+      title
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNote = /* GraphQL */ `
+  subscription OnUpdateNote {
+    onUpdateNote {
+      id
+      title
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNote = /* GraphQL */ `
+  subscription OnDeleteNote {
+    onDeleteNote {
+      id
+      title
+      body
+      createdAt
+      updatedAt
+    }
+  }
+`;
