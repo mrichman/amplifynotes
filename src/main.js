f1cb8256d8c35d89556dfe98510e857bdc261f31
Mark Richman <mrkrchm@amazon.com>
2021-04-14 10:04:34 -0400

    initial commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import {
+  applyPolyfills,
+  defineCustomElements,
+} from '@aws-amplify/ui-components/loader';
+
+Amplify.configure(aws_exports);
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
+
+createApp(App).mount('#app')
